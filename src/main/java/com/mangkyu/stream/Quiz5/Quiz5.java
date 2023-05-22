c23bbe681ffaf6982ca3b8134d8deb1c216d94a7
LEEHYUNDONG <l.steve10@yahoo.com>
2023-05-22 14:27:37 +0900

    Quiz4

--- src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
+++ src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
@@ -1,5 +1,6 @@
 package com.mangkyu.stream.Quiz5;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,6 +10,10 @@ public class Quiz5 {
 
     // 5.1 모든 문자열의 길이를 더한 결과를 출력하여라.
     public int quiz1() {
+        int res = Arrays.stream(STRING_ARR)
+                .map(s -> s.length())
+                .reduce(Integer::sum)
+                ;
         return 0;
     }
 
@@ -19,6 +24,7 @@ public class Quiz5 {
 
     // 5.3 임의의 로또번호(1~45)를 정렬해서 출력하시오.
     public List<Integer> quiz3() {
+
         return Collections.emptyList();
     }
 
